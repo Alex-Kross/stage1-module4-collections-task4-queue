ee13b8376507445c8905a944b7d3358d339bc314
Alex-Kross <artemkirlanovia@gmail.com>
2023-01-22 19:46:39 +0300

    task completed

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,35 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        Queue<Integer> queue = new PriorityQueue<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.add(i);
+        }
+        int fullSizeQueue = queue.size();
+        int sizeNewQueue = fullSizeQueue;
+        int index = 1;
+        for (int i = 0; i < fullSizeQueue; i++) {
+            Queue<Integer> queue2 = new PriorityQueue<>();
+            for (int j = 1; j <= sizeNewQueue; j++) {
+                if (index % everyDishNumberToEat == 0) {
+                    list.add(queue.remove());
+                    index = 1;
+                } else {
+                    queue2.add(queue.remove());
+                    index++;
+                }
+            }
+            sizeNewQueue = queue2.size();
+            queue.clear();
+            queue.addAll(queue2);
+        }
+        return list;
     }
 }
--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,27 @@
 package com.epam.collections.queue;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        PriorityQueue<String> queue = new PriorityQueue<>(new SortComparator());
+
+        for (String el : firstList) {
+            queue.add(el);
+        }
+        for (String el : secondList) {
+            queue.add(el);
+        }
+        return queue;
     }
 }
+
+class SortComparator implements Comparator<String>{
+    @Override
+    public int compare(String o1, String o2) {
+        return -o1.compareTo(o2);
+    }
+}
+
